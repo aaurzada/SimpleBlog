167b146b9b44813941f3d36ae459257af279c4e6
Andrew Aurzada <aaurzada@gmail.com>
2015-01-20 19:13:38 -0600

    Added basic login form.

--- SimpleBlog/Controllers/AuthController.cs
+++ SimpleBlog/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SimpleBlog.ViewModels;
 
 namespace SimpleBlog.Controllers
 {
@@ -10,7 +11,24 @@ namespace SimpleBlog.Controllers
     {
         public ActionResult Login()
         {
-            return View();
+            return View(new AuthLogin 
+            { 
+            });
+        }
+
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+            if (!ModelState.IsValid)
+                return View(form);
+
+            if (form.Username != "Andrew Aurzada")
+            {
+                ModelState.AddModelError("Username", "Username or password is incorrect.");
+                return View(form);
+            }
+
+            return Content("Hey there, " + form.Username);
         }
     }
 }
